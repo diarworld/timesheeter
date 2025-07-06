de5dc35e510de00abf71d0f84cbe01c6ec2d535a
Dmitry Ibragimov <dmitry.ibragimov@lemanapro.ru>
2025-07-07 01:44:59 +0300

    Team management refactoring

--- src/entities/track/common/lib/validate-ldap.ts
+++ src/entities/track/common/lib/validate-ldap.ts
@@ -2,7 +2,7 @@ import { LDAP_REGEX } from 'entities/track/common/lib/constants';
 
 export const validateLDAP = (str: string | undefined) => {
   if (!str) {
-    return false;
+    return true;
   }
 
   return !!str && str.match(LDAP_REGEX) !== null;
