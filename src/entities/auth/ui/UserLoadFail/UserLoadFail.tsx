b343555079385fb07784ab3e56d19beefd69d0e9
Dmitry Ibragimov <dmitry.ibragimov@lemanapro.ru>
2025-07-08 19:19:56 +0300

    Fix for Request Header Or Cookie Too Large yandex response, update docs, add docs theme

--- src/entities/auth/ui/UserLoadFail/UserLoadFail.tsx
+++ src/entities/auth/ui/UserLoadFail/UserLoadFail.tsx
@@ -3,12 +3,30 @@ import { Message } from 'entities/locale/ui/Message';
 import { DeleteOutlined } from '@ant-design/icons';
 import Cookies from 'js-cookie';
 
+function getAllUpperDomains(hostname: string) {
+  const parts = hostname.split('.');
+  const domains = [];
+  for (let i = 0; i <= parts.length - 2; i++) {
+    domains.push('.' + parts.slice(i).join('.'));
+  }
+  return domains;
+}
+
+function deleteAllCookiesForAllDomains() {
+  const currentDomain = window.location.hostname;
+  const allDomains = getAllUpperDomains(currentDomain);
+  const allCookies = Cookies.get();
+  Object.keys(allCookies).forEach((cookieName) => {
+    Cookies.remove(cookieName, { path: '/' });
+    allDomains.forEach(domain => {
+      Cookies.remove(cookieName, { path: '/', domain });
+    });
+  });
+}
+
 const handleClearCookies = () => {
-    const allCookies = Cookies.get(); 
-    for (const cookieName in allCookies) {
-        Cookies.remove(cookieName);
-    }
-    window.location.reload();
+  deleteAllCookiesForAllDomains();
+  window.location.reload();
 };
 
 export const UserLoadFail = () => (
