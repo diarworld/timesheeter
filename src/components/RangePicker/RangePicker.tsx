848095c0e424bb744f85e16577a18e317a5748a7
Dmitry Ibragimov <dmitry.ibragimov@lemanapro.ru>
2025-07-18 02:02:53 +0300

    Fix colors for datepicker dark

--- src/components/RangePicker/RangePicker.tsx
+++ src/components/RangePicker/RangePicker.tsx
@@ -1,9 +1,10 @@
 import type { RangePickerProps } from 'antd/lib/date-picker';
 import { DatePicker } from 'antd';
+import clsx from 'clsx';
 import styles from './RangePicker.module.scss';
 
-export const RangePicker = (props: RangePickerProps) => (
-  <div className={styles.wrapper}>
+export const RangePicker = (props: RangePickerProps & { isDarkMode: boolean }) => (
+  <div className={clsx(styles.wrapper, { [styles.wrapper_dark]: props.isDarkMode }, { [styles.wrapper_light]: !props.isDarkMode })}>
     <DatePicker.RangePicker {...props} getPopupContainer={(trigger) => trigger} />
   </div>
 );
